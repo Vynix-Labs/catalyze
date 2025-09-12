9884c6e630fcfe641bca484c9afc1bcbfa0246f6
raymondjoseph02 <amraymondjoseph@gmail.com>
2025-09-13 00:01:16 +0100

    feat: implement the onboarding flow ui

---
+++ src/common/ui/button.tsx
@@ -0,0 +1,32 @@
+import type { buttonProps } from "../../types/types";
+
+function Button({
+  variants,
+  text,
+  classes,
+  children,
+  handleClick,
+}: buttonProps) {
+  const btnShape = () => {
+    switch (variants) {
+      case "primary":
+        return "bg-primary-100 text-base font-bold hover:opacity-88  transition ease-out duration-300 leading-[132%] text-white px-3 py-2 rounded"; // example Tailwind classes
+
+      case "secondary":
+        return "bg-neutral-100 text-gray-600 px-4 py-2";
+
+      default:
+        return "bg-transparent text-black px-4 py-2"; // fallback
+    }
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={`${btnShape()} ${classes} gap-2 h-12 max-w-[21.9rem] flex w-full rounded-full items-center justify-center cursor-pointer`}
+    >
+      {children ? children : text}
+    </button>
+  );
+}
+export default Button;
---
+++ src/types/types.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from "react";
+
+export interface buttonProps {
+  variants: "primary" | "secondary"; // add more if need and button ;
+  classes?: string;
+  children?: ReactNode;
+  text?: string;
+  handleClick?: () => void;
+}
+export interface AuthHeaderProps {
+  title: string;
+  description: string;
+  link?: {
+    url: string;
+    text: string;
+  };
+  isLink: boolean;
+}
