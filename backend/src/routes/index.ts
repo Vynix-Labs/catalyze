8ba59e3732e61bd36928ba03a86d93aee2799f98
Ajibose <ajiboseibrahim12@gmail.com>
2025-09-29 12:04:25 +0100

    Implemented transaction endpoints

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,20 +1,22 @@
 import type { FastifyPluginAsync } from 'fastify'
 import authRoutes from '../modules/auth/auth.routes'
+import transactionsRoutes from '../modules/transactions/transactions.routes'
 /* 
 import cryptoRoutes from '../modules/crypto/crypto.routes'
 import fiatRoutes from '../modules/fiat/fiat.routes'
 import stakingRoutes from '../modules/staking/staking.routes'
-import transactionsRoutes from '../modules/transactions/transactions.routes'
+
 import usersRoutes from '../modules/users/users.routes' 
 */
 
 const routes: FastifyPluginAsync = async (fastify) => {
   fastify.register(authRoutes, { prefix: '/auth' })
+  fastify.register(transactionsRoutes, { prefix: '' })
   /*
   fastify.register(cryptoRoutes, { prefix: '/crypto' })
   fastify.register(fiatRoutes, { prefix: '/fiat' })
   fastify.register(stakingRoutes, { prefix: '/staking' })
-  fastify.register(transactionsRoutes, { prefix: '/transactions' })
+  
   fastify.register(usersRoutes, { prefix: '/users' }) */
 
   // Root endpoint
