cf8501e0a92ee50cbed1d33fa752e29847a4d86d
best2025j <audugodstime95@gmail.com>
2025-10-01 23:24:48 +0100

    changes made

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,21 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+<<<<<<< Updated upstream
 // https://vite.dev/config/
+=======
+>>>>>>> Stashed changes
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(), // This should come after react()
+  ],
   server: {
     host: "localhost",
     port: 5173,
     cors: true,
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
   },
 });
