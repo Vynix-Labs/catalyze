acea64ba2898eb89b3bc619b1d835adb078ee506
best2025j <audugodstime95@gmail.com>
2025-10-01 23:25:45 +0100

    Resolve merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-<<<<<<< Updated upstream
-// https://vite.dev/config/
-=======
->>>>>>> Stashed changes
 export default defineConfig({
   plugins: [
     react(),
